c2828e60e26892dbf2e783fa586c612e2f13ec1b
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-05-15 09:09:04 -0300

    Update translation service, replacing ngx/translate with transloco

--- projects/core/src/lib/services/translation/translation.service.ts
+++ projects/core/src/lib/services/translation/translation.service.ts
@@ -3,76 +3,62 @@ import { Injectable } from '@angular/core';
 
 import { GeolocationLanguageModel, getGeolocationTranslation } from '../../models/geolocation/geolocation-language.model';
 import { GeolocationDataModel } from '../../models/geolocation/geolocation-data.model';
-import { GeolocationService } from '../geolocation/geolocation.service';
 import { NotificationService } from '../notification/notification.service';
-import { TranslateService } from '@ngx-translate/core';
+import { GeolocationService } from '../geolocation/geolocation.service';
+import { TranslocoService } from '@ngneat/transloco';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class TranslationService {
-  constructor(
-    private readonly translateService: TranslateService,
-    private readonly geolocationService: GeolocationService,
-    private readonly titleService: Title,
-    private readonly notificationService: NotificationService
-  ) {}
-
-  public addLangs(): void {
-    this.translateService.addLangs(['de-DE', 'en-US', 'es-ES', 'fr-FR', 'pt-BR', 'ru-RU', 'zh-CN']);
-  }
+  constructor(private readonly notificationService: NotificationService,
+              private readonly geolocationService: GeolocationService,
+              private readonly translocoService: TranslocoService,
+              private readonly titleService: Title) { }
 
   public initializeLanguage(): void {
-    if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-      const cachedLang: string | null = localStorage.getItem('userLang');
-      if (cachedLang == null) {
-        this.setLanguageByGeolocation().then(() => {});
-      } else {
-        this.translateService.use(cachedLang);
-      }
-    } else {
-      this.translateService.use(GeolocationLanguageModel.ENGLISH.toString());
+    const cachedLang = localStorage.getItem('userLang');
+    const lang = cachedLang ?? GeolocationLanguageModel.ENGLISH;
+
+    this.translocoService.setActiveLang(lang);
+    if (!cachedLang) {
+      this.setLanguageByGeolocation();
     }
   }
 
-  public async setLanguageByGeolocation(): Promise<void> {
+  public setLanguageByGeolocation(): void {
     this.geolocationService.getGeolocation().subscribe({
       next: (data: GeolocationDataModel) => {
         const userLang = getGeolocationTranslation(data.countryCode);
-        this.translateService.use(userLang);
+        this.translocoService.setActiveLang(userLang);
         localStorage.setItem('userLang', userLang);
       },
-      error: (error: any) => {
-        this.translateService.use(GeolocationLanguageModel.ENGLISH.toString());
-        localStorage.setItem('userLang', GeolocationLanguageModel.ENGLISH.toString());
-        throw new Error('Error determining location: ' + error.message);
+      error: () => {
+        this.translocoService.setActiveLang(GeolocationLanguageModel.ENGLISH);
+        localStorage.setItem('userLang', GeolocationLanguageModel.ENGLISH);
       }
     });
   }
 
   public defineTitle(key: string): void {
-    this.translateService.get(key).subscribe((translatedTitle: string) => {
-      this.titleService.setTitle(translatedTitle);
+    this.translocoService.selectTranslate(key).subscribe(title => {
+      this.titleService.setTitle(title);
     });
   }
 
-  public translateNotificationSuccess(translationKey: string, message?: string): void {
-    this.translateService.get(translationKey).subscribe(translation => {
-      const fullMessage = message ? `${translation} ${message}` : translation;
-      this.notificationService.success(fullMessage);
+  public translateNotificationSuccess(key: string, message?: string): void {
+    this.translocoService.selectTranslate(key).subscribe(translation => {
+      this.notificationService.success(message ? `${translation} ${message}` : translation);
     });
   }
 
-  public translateNotificationWarning(translationKey: string): void {
-    this.translateService.get(translationKey).subscribe(translation => {
+  public translateNotificationWarning(key: string): void {
+    this.translocoService.selectTranslate(key).subscribe(translation => {
       this.notificationService.warning(translation);
     });
   }
 
-  public translateNotificationError(translationKey: string, errorMessage?: string): void {
-    this.translateService.get(translationKey).subscribe(translation => {
-      const fullMessage = errorMessage ? `${translation} ${errorMessage}` : translation;
-      this.notificationService.error(fullMessage);
+  public translateNotificationError(key: string, errorMessage?: string): void {
+    this.translocoService.selectTranslate(key).subscribe(translation => {
+      this.notificationService.error(errorMessage ? `${translation} ${errorMessage}` : translation);
     });
   }
 }
