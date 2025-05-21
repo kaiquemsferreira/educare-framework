30eb7af843ad326f794403ea712866778a61b331
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-05-21 11:21:26 -0300

    Remove image-slider

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -1,6 +1,5 @@
 export * from './lib/config/api.config';
 
-
 export * from './lib/services/api/api.service';
 export * from './lib/services/geolocation/geolocation.service';
 export * from './lib/services/screen-size/screen-size.service';
@@ -13,8 +12,6 @@ export * from './lib/services/notification/components/success/success.component'
 export * from './lib/services/notification/components/warning/warning.component';
 export * from './lib/services/notification/components/error/error.component';
 export * from './lib/components/loading-spinner/loading-spinner.component';
-export * from './lib/components/image-slider/image-slider.component';
-export * from './lib/components/shared-components.module';
 
 export * from './lib/models/geolocation/geolocation-language.model';
 export * from './lib/models/geolocation/geolocation-data.model';
