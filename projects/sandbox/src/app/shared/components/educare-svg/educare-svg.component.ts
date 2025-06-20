c5559be79c07712e50c25c24d2a58f97fd88a58b
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-06-20 07:36:23 -0300

    create a educare svg component

---
+++ projects/sandbox/src/app/shared/components/educare-svg/educare-svg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'educare-svg',
+  imports: [],
+  templateUrl: './educare-svg.component.html',
+  styleUrl: './educare-svg.component.scss'
+})
+export class EducareSvgComponent {
+
+}
