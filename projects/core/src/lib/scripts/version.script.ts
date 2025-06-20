5331786d9dea32c56b2469ebc402014e76cc4713
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-06-20 07:36:44 -0300

    create a version script

---
+++ projects/core/src/lib/scripts/version.script.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+
+const packageJsonPath = path.resolve(__dirname, '../../package.json');
+const versionFilePath = path.resolve(__dirname, '../version.ts');
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const version = packageJson.version;
+
+fs.writeFileSync(versionFilePath, `export const version = '${version}';\n`);
+
+console.log(`✔ Version ${version} written em version.ts`);
